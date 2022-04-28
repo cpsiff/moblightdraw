1cd7cce6122dbc554e80087e4dfc7b5198fb0e4b
Carter Sifferman <cpsiff@gmail.com>
2022-04-28 14:23:59 -0500

    set_leds

---
+++ src/set_leds.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import rospy
+import numpy as np
+from mobrob_util.msg import ME439Color
+import qwiic_led_stick
+import sys
+   
+
+def talker():
+    sub_color = rospy.Subscriber('/led_color', ME439Color, set_led_color)  
+
+    if my_stick.begin() == False:
+        print("\nThe Qwiic LED Stick isn't connected to the sytsem. Please check your connection", \
+            file=sys.stderr)
+        return
+
+    print("\nLED Stick ready!")    
+
+    my_stick.set_all_LED_brightness(15)
+    
+
+def set_led_color(msg_in):
+    global my_stick
+
+    my_stick.set_all_LED_color(int(msg_in.red), int(msg_in.green), int(msg_in.blue))
+
+
+my_stick = qwiic_led_stick.QwiicLEDStick()
+
+if __name__ == '__main__':
+    try: 
+        talker()
+    except rospy.ROSInterruptException: 
+        pass
